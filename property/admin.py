3b557561406b505086326e35c9f52f794ae6c700
Artem <zinykov_a@mail.ru>
2024-06-20 20:53:22 +0300

    Изменил регистрацию моделей в админке на @admin.register

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInline(admin.TabularInline):
     raw_id_fields = ("owner", )
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner',)
     readonly_fields = ("created_at", )
@@ -28,15 +29,11 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerInline]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ("flat", "author")
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ("owner_flats", )
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
-
